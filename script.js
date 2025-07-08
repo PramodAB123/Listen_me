12ee5effe0d208a953bbc18805013d850e4b7df4
PramodAB123 <pramodbkl111@gmail.com>
2025-07-08 19:19:40 +0530

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
