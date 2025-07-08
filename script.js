0a1cb8d2438c5bb56364f2d241e7d898ac68220e
PramodAB123 <pramodbkl111@gmail.com>
2025-07-08 22:35:43 +0530

    sung

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-
 let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar');
+let myProgressBar = document.getElementById('myprogressBar');
 let gif = document.getElementById('gif');
-let masterSongName = document.getElementById('masterSongName');
-let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.add('fa-pause');
+        masterPlay.classList.remove('fa-circle-play');
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause');
+        masterPlay.classList.add('fa-circle-play');
+    }
+})
+
+audioElement.addEventListener('timeupdate', () => {
+    if (audioElement.duration) {
+        const percent = (audioElement.currentTime / audioElement.duration) * 100;
+        myProgressBar.value = percent;
+
+    }
+});
